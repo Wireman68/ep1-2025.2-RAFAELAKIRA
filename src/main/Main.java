699bb48f26f9bbff3f6406ea4ac6a3533bbfaf02
Boyle <51026484+Wireman68@users.noreply.github.com>
2025-10-05 21:05:38 -0300

    atualizado main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,39 +1,16 @@
 package main;
 
 import main.bancos.BancoDeDados;
-import main.core.RegistroEventos;
-import main.registro.Paciente;
 
-import java.util.Scanner;
+import static main.menus.MenuLogico.menu;
 
 public class Main
 {
     private static boolean EXIT_CHAVE = false;
+    public static BancoDeDados db = new BancoDeDados();
 
     public static void main(String[] args)
     {
-        BancoDeDados bancoDeDados = new BancoDeDados();
-        RegistroEventos.initBancoDeDados(bancoDeDados);
-        do {
-
-            Scanner scanner = new Scanner(System.in);
-
-            Paciente pacienteTeste = new Paciente(scanner.nextLine(), scanner.nextLine(), scanner.nextInt());
-
-            bancoDeDados.registrar(pacienteTeste, false);
-
-            System.out.println("dados do paciente:");
-            pacienteTeste.displayDados();
-
-            System.out.println("0 para continuar, outro para acabar");
-            EXIT_CHAVE = scanner.nextInt() != 0;
-
-        } while(!EXIT_CHAVE);
-
-        Runtime.getRuntime().addShutdownHook(new Thread(() ->
-        {
-            System.out.println("Salvando dados...");
-            RegistroEventos.salvarDeferidos(bancoDeDados);
-        }));
+        menu(db);
     }
 }
